5f69e14e61d03ab37802d026c8f4fd6770fe7136
luckysharms775 <arjun.sharma1229@gmail.com>
2019-06-27 16:12:35 -0500

    fixed window

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,11 @@
-
+//import exampleVideoData from '../exampleVideoData/';
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
   }
   render() {
+    console.log(exampleVideoData);
     return (
       <div>
         <nav className="navbar">
@@ -27,3 +28,4 @@ export default class App extends React.Component {
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
+window.App = App;
