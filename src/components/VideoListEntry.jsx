14fea5ae82c7f2f609c8390af361c6a38a44d632
luckysharms775 <arjun.sharma1229@gmail.com>
2019-06-27 15:34:48 -0500

    spaces matter

--- src/components/VideoListEntry.jsx
+++ src/components/VideoListEntry.jsx
@@ -3,11 +3,11 @@ const VideoListEntry = ({video}) =>
   (
     <div className="video-list-entry media">
       <div className="media-left media-middle">
-        <img className="media-object" src="https://i.ytimg.com/vi/1w8Z0UOXVaY/default.jpg" alt="" />
+        <img className="media-object" src={video.snippet.thumbnails.default.url} alt="" />
       </div>
       <div className="media-body">
-        <div className="video-list-entry-title">Video Title {props.video.snippet.title} </div>
-        <div className="video-list-entry-detail">Video Description{props.video.snippet.description} </div>
+        <div className="video-list-entry-title">{video.snippet.title}</div>
+        <div className="video-list-entry-detail">{video.snippet.description}</div>
       </div>
     </div>
   );
